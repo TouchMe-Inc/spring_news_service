e5352f84b2bb5efcedb70ca02ac12afff921a24d
Dmitry Pavlukov <dmitry.pavlukov@gmail.com>
2023-08-18 20:07:48 +0300

    add cache in news controller

--- src/main/java/by/touchme/newsservice/controller/NewsController.java
+++ src/main/java/by/touchme/newsservice/controller/NewsController.java
@@ -1,47 +1,72 @@
 package by.touchme.newsservice.controller;
 
+import by.touchme.newsservice.cache.Cache;
+import by.touchme.newsservice.cache.impl.LRUCache;
 import by.touchme.newsservice.entity.News;
 import by.touchme.newsservice.service.NewsService;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-@AllArgsConstructor
+import java.util.Optional;
+
+
 @RequestMapping("/v1/news")
 @RestController
 public class NewsController {
 
-    private NewsService newsService;
+    private final NewsService newsService;
+    private final Cache<Long, News> cache;
+
+    NewsController(NewsService newsService) {
+        this.newsService = newsService;
+        this.cache = new LRUCache<>(10);
+    }
 
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public News getById(@PathVariable(name = "id") Long id) {
-        return newsService.getById(id);
+        News news = this.cache.get(id)
+                .orElse(this.newsService.getById(id));
+
+        this.cache.set(id, news);
+
+        return news;
     }
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public Page<News> getPage(Pageable pageable) {
-        return newsService.getPage(pageable);
+        return this.newsService.getPage(pageable);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public News create(@RequestBody News news) {
-        return newsService.create(news);
+        News createdNews = this.newsService.create(news);
+
+        this.cache.set(createdNews.getId(), createdNews);
+
+        return createdNews;
     }
 
     @PutMapping(value = "/{id}")
     @ResponseStatus(HttpStatus.OK)
     public News updateById(@PathVariable(name = "id") Long id, @RequestBody News news) {
-        return newsService.updateById(id, news);
+        News updatedNews = this.newsService.updateById(id, news);
+
+        this.cache.set(id, updatedNews);
+
+        return updatedNews;
     }
 
     @DeleteMapping(value = "/{id}")
     @ResponseStatus(HttpStatus.OK)
     public void deleteById(@PathVariable(name = "id") Long id) {
-        newsService.deleteById(id);
+        this.newsService.deleteById(id);
+        this.cache.set(id, null);
     }
 }
