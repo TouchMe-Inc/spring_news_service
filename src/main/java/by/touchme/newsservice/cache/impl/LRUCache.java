beddb97cfb7e23fa48f42d3cd08d3f7a5012b522
Dmitry Pavlukov <dmitry.pavlukov@gmail.com>
2023-08-18 19:37:28 +0300

    Added thread-safe implementation of LRUCache

--- src/main/java/by/touchme/newsservice/cache/impl/LRUCache.java
+++ src/main/java/by/touchme/newsservice/cache/impl/LRUCache.java
@@ -2,28 +2,73 @@ package by.touchme.newsservice.cache.impl;
 
 import by.touchme.newsservice.cache.Cache;
 
-import java.util.Optional;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class LRUCache<K, V> implements Cache<K, V> {
-    private int capacity;
+    private final int capacity;
+
+    private final Map<K, V> data;
+
+    private final LinkedList<K> order;
+
+    private final ReentrantReadWriteLock lock;
 
     public LRUCache(int capacity) {
         this.capacity = capacity;
+        this.data = new ConcurrentHashMap<>(capacity);
+        this.order = new LinkedList<>();
+        this.lock = new ReentrantReadWriteLock();
     }
 
     @Override
-    public boolean set(K key, V value) {
-        return false;
+    public void set(K key, V value) {
+        this.lock.writeLock().lock();
+
+        try {
+            if (this.data.containsKey(key)) {
+                this.removeKey(key);
+            } else if (this.size() >= this.capacity) {
+                K keyRemoved = this.order.removeLast();
+                this.removeKey(keyRemoved);
+            }
+
+            this.data.put(key, value);
+            this.order.addFirst(key);
+        } finally {
+            this.lock.writeLock().unlock();
+        }
     }
 
     @Override
     public Optional<V> get(K key) {
-        return Optional.empty();
+        this.lock.readLock().lock();
+
+        try {
+            V value = this.data.get(key);
+
+            if (value != null) {
+                this.order.remove(key);
+                this.order.addFirst(key);
+
+                return Optional.of(value);
+            }
+
+            return Optional.empty();
+        } finally {
+            this.lock.readLock().unlock();
+        }
     }
 
     @Override
     public int size() {
-        return 0;
+        this.lock.readLock().lock();
+        try {
+            return this.order.size();
+        } finally {
+            this.lock.readLock().unlock();
+        }
     }
 
     @Override
@@ -33,6 +78,18 @@ public class LRUCache<K, V> implements Cache<K, V> {
 
     @Override
     public void clear() {
+        this.lock.writeLock().lock();
 
+        try {
+            this.data.clear();
+            this.order.clear();
+        } finally {
+            this.lock.writeLock().unlock();
+        }
+    }
+
+    private void removeKey(K key) {
+        this.data.remove(key);
+        this.order.remove(key);
     }
 }
