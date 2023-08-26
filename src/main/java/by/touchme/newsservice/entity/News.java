57b94045ff04d63f46e3e611888c99140f082d1a
Dmitry Pavlukov <dmitry.pavlukov@gmail.com>
2023-08-26 09:37:53 +0300

    add javadoc for news entity

--- src/main/java/by/touchme/newsservice/entity/News.java
+++ src/main/java/by/touchme/newsservice/entity/News.java
@@ -6,21 +6,36 @@ import org.hibernate.annotations.CreationTimestamp;
 
 import java.util.Date;
 
+/**
+ * The news object contains main details about News.
+ */
 @Data
 @Table(name = "news")
 @Entity
 public class News {
 
+    /**
+     * Unique identifier of the news.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    /**
+     * Title of the news.
+     */
     @Column(name = "title", nullable = false)
     private String title;
 
+    /**
+     * Main content of the news.
+     */
     @Column(name = "text", nullable = false, columnDefinition = "TEXT")
     private String text;
 
+    /**
+     * Time of creation of the news.
+     */
     @CreationTimestamp
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "time")
