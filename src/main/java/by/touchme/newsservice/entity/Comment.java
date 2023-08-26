d863f6b431a10b166ad7149b065e95606ba4343c
Dmitry Pavlukov <dmitry.pavlukov@gmail.com>
2023-08-26 09:37:43 +0300

    add javadoc for comment entity

--- src/main/java/by/touchme/newsservice/entity/Comment.java
+++ src/main/java/by/touchme/newsservice/entity/Comment.java
@@ -6,26 +6,45 @@ import org.hibernate.annotations.CreationTimestamp;
 
 import java.util.Date;
 
+/**
+ * The comment object contains main details about Comment.
+ */
 @Data
 @Table(name = "comments")
 @Entity
 public class Comment {
 
+    /**
+     * Unique identifier of the comment.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    /**
+     * Author of the comment.
+     */
     @Column(name = "username", nullable = false)
     private String username;
 
+    /**
+     * Message of the comment.
+     */
     @Column(name = "text", nullable = false, columnDefinition = "TEXT")
     private String text;
 
+
+    /**
+     * Time of creation of the news.
+     */
     @CreationTimestamp
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "time")
     private Date time;
 
+    /**
+     * The identifier of the news the comment is associated with.
+     */
     @Column(name = "news_id")
-    private Long newsId;
+    private Long newsId; // TODO: OneToMany relation?
 }
