e29b2b78469d83e7295aaf76d3b244292da2ed7c
Dmitry Pavlukov <dmitry.pavlukov@gmail.com>
2023-08-26 10:02:43 +0300

    add javadoc for exception descriptions

--- src/main/java/by/touchme/newsservice/exception/NewsNotFoundException.java
+++ src/main/java/by/touchme/newsservice/exception/NewsNotFoundException.java
@@ -1,9 +1,23 @@
 package by.touchme.newsservice.exception;
 
+/**
+ * The class {@code NewsNotFoundException} and its subclasses
+ * are a form of {@code RuntimeException} that indicates conditions
+ * that a reasonable application might want to catch.
+ */
 public class NewsNotFoundException extends RuntimeException {
 
+    /**
+     * Unique identifier of the news.
+     */
     private final Long newsId;
 
+    /**
+     * Constructs a new runtime exception with the news identifier.
+     *
+     * @param   newsId   the news identifier. The news identifier is saved for
+     *          later retrieval by the {@link #getMessage()} method.
+     */
     public NewsNotFoundException(Long newsId) {
         this.newsId = newsId;
     }
