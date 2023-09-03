30f6a0322669d0ba93d66c634116add3eb4126ed
Dmitry Pavlukov <dmitry.pavlukov@gmail.com>
2023-09-03 23:15:36 +0300

    clear import for comment mapper

--- src/main/java/by/touchme/newsservice/mapper/CommentMapper.java
+++ src/main/java/by/touchme/newsservice/mapper/CommentMapper.java
@@ -1,9 +1,7 @@
 package by.touchme.newsservice.mapper;
 
 import by.touchme.newsservice.dto.CommentDto;
-import by.touchme.newsservice.dto.NewsDto;
 import by.touchme.newsservice.entity.Comment;
-import by.touchme.newsservice.entity.News;
 import org.mapstruct.Mapper;
 
 import java.util.List;
